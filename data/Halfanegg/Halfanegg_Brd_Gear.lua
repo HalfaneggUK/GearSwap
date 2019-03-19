ab14b80c39ab8840d5abecf6ff491c5c172a0e39
HalfaneggUK <47830264+HalfaneggUK@users.noreply.github.com>
2019-03-19 22:05:11 +0000

    Weapon set error fixed

--- data/Halfanegg/Halfanegg_Brd_Gear.lua
+++ data/Halfanegg/Halfanegg_Brd_Gear.lua
@@ -3,7 +3,7 @@ function user_setup()
     state.OffenseMode:options('Normal','Acc')
     state.CastingMode:options('Normal','Resistant','AoE')
     state.IdleMode:options('Normal','Refresh','Melee')
-	  state.Weapons:options('Carnwenhan','TwashTP','TwashACC', 'CarnKali')
+	  state.Weapons:options('Carnwenhan','TwashTP','TwashAcc', 'CarnKali')
 
 	-- Adjust this if using the Terpander (new +song instrument)
     info.ExtraSongInstrument = 'Daurdabla'
